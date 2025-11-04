e17b62d24c6d206c2574c00de5be1012cec3522f
yangfeng <yangfeng@tenorshare.cn>
2025-11-04 15:47:14 +0800

    Initial Commit

---
+++ SegDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SegDemo
+//
+//  Created by yf on 2025/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SegDemo/SegDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegDemoApp.swift
+//  SegDemo
+//
+//  Created by yf on 2025/11/4.
+//
+
+import SwiftUI
+
+@main
+struct SegDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
